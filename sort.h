cab84c4a423cdeacbab6528fd3e6511f276caad2
salmalah <s.salmalahmidi@gmail.com>
2023-06-27 10:38:23 -0700

    33

--- sort.h
+++ sort.h
@@ -22,6 +22,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
