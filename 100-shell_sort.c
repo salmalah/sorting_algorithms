2a061832ad1e50721d46b21df74d7631fb56bc8c
salmalah <s.salmalahmidi@gmail.com>
2023-06-27 06:34:27 -0700

    test

---
+++ 100-shell_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * shell_sort - A function sorts integers in ascending order using Shell sort
+ * @array: The array
+ * @size:size of array
+ * Return: Nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+	int n;
+	size_t g, k, m;
+
+	g = 1;
+	if (!array || size < 2)
+		return;
+	while (g < size)
+		g = g * 3 + 1;
+	g = (g - 1) / 3;
+	while (g > 0)
+	{
+		for (k = g; k < size; k++)
+		{
+			n = array[k];
+			for (m = k; m >= g && array[m - g] > n; m -= g)
+			{
+				array[m] = array[m - g];
+			}
+			array[m] = n;
+		}
+		print_array(array, size);
+
+		g = (g - 1) / 3;
+	}
+}
+
--- sort.h
+++ sort.h
@@ -20,5 +20,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_list(const listint_t *list);
+void shell_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 #endif
